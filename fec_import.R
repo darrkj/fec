1d0d50e701bcc3f3e3e6be122f17f59620def952
Kenny Darrell <darrelkj@gmail.com>
2015-09-12 09:29:30 -0400

    all years are now a function call

--- fec_import.R
+++ fec_import.R
@@ -3,46 +3,28 @@ library(dplyr)
 # Data comes from
 # http://www.fec.gov/finance/disclosure/ftpdet.shtml
 
-function(str) {
-str %>%
-    paste0('http://www.fec.gov/finance/disclosure/metadata/', ., '_header_file.csv') %>%
-    read.csv()
-
-CCL_NAMES <- names(read.csv())
-
-list.files('data') %>%
-  grep('ccl', ., value = T) %>%
-  grep('txt', ., value = T) %>%
-  paste0('data/', .) %>%
-  lapply(read.csv, sep = '|', header = F) %>%
-  bind_rows %>%
-  `names<-`(CCL_NAMES) -> link
-
+fec_data_ingest <- function(str) {
+  str %>%
+    #paste0('http://www.fec.gov/finance/disclosure/metadata/', ., '_header_file.csv') %>%
+    paste0('names/', ., '_header_file.csv') %>%
+    read.csv() %>% 
+    names -> data_names
+  
+  list.files('data') %>%
+    grep('ccl', ., value = T) %>%
+    grep('txt', ., value = T) %>%
+    paste0('data/', .) %>%
+    lapply(read.csv, sep = '|', header = F) %>%
+    bind_rows %>%
+    `names<-`(data_names)
 }
-ccl_header_file
-cm_header_file.csv
-cn_header_file
-oth_header_file
-pas2_header_file
-indiv_header_file
-oppexp_header_file
+
 
 
 # The individual contributions file contains each contribution from an 
 # individual to a federal committee if the contribution was at least $200.
-list.files('data') %>%
-  grep('indiv', ., value = T) %>%
-  grep('txt', ., value = T) %>%
-  paste0('data/', .) -> files
 
-indiv <- lapply()
-indiv <- read.csv('2016/itcont.txt', sep = '|', header = F)
-
-names(indiv) <- c('CMTE_ID', 'AMNDT_IND', 'RPT_TP', 'TRANSACTION_PGI',
-               'IMAGE_NUM', 'TRANSACTION_TP', 'ENTITY_TP', 'NAME', 
-               'CITY', 'STATE', 'ZIP_CODE', 'EMPLOYER', 'OCCUPATION',
-               'TRANSACTION_DT', 'TRANSACTION_AMT', 'OTHER_ID',
-               'TRAN_ID', 'FILE_NUM', 'MEMO_CD', 'MEMO_TEXT', 'SUB_ID')
+indiv <- fec_data_ingest('itcont')
 
 
 # The contributions to committees from committees file contains each 
@@ -50,16 +32,7 @@ names(indiv) <- c('CMTE_ID', 'AMNDT_IND', 'RPT_TP', 'TRANSACTION_PGI',
 # candidate committee, or other federal committee to another committee during 
 # the two-year election cycle.
 
-cm2cm <- read.csv('2016/itoth.txt', sep = '|', header = F)
-
-names(cm2cm) <- c('CMTE_ID', 'AMNDT_IND', 'RPT_TP', 'TRANSACTION_PGI',
-               'IMAGE_NUM', 'TRANSACTION_TP', 'ENTITY_TP', 'NAME',
-               'CITY', 'STATE', 'ZIP_CODE', 'EMPLOYER',
-               'OCCUPATION', 'TRANSACTION_DT', 'TRANSACTION_AMT',
-               'OTHER_ID', 'TRAN_ID', 'FILE_NUM', 'MEMO_CD',
-               'MEMO_TEXT', 'SUB_ID')
-
-
+cm2cm <- fec_data_ingest('itoth') 
 
 # The contributions to candidates from committees file contains each 
 # contribution or independent expenditure made by a PAC, party committee, 
@@ -67,15 +40,7 @@ names(cm2cm) <- c('CMTE_ID', 'AMNDT_IND', 'RPT_TP', 'TRANSACTION_PGI',
 # two-year election cycle.
 
 
-cm2cd <- read.csv('2016/itpas2.txt', sep = '|', header = F)
-
-names(cm2cd) <- c('CMTE_ID', 'AMNDT_IND', 'RPT_TP', 'TRANSACTION_PGI',
-               'IMAGE_NUM', 'TRANSACTION_TP', 'ENTITY_TP',
-               'NAME', 'CITY', 'STATE', 'ZIP_CODE',
-               'EMPLOYER', 'OCCUPATION', 'TRANSACTION_DT',
-               'TRANSACTION_AMT', 'OTHER_ID', 'CAND_ID', 'TRAN_ID',
-               'FILE_NUM', 'MEMO_CD', 'MEMO_TEXT', 'SUB_ID')
-
+cm2cd <- fec_data_ingest('itpas2')
 
 # The Operating Expenditures file contains disbursements reported on FEC 
 # Form 3 Line 17, FEC Form 3P Line 23and FEC Form 3X Lines 21(a)(i), 21(a)(ii) 
@@ -86,31 +51,12 @@ names(cm2cd) <- c('CMTE_ID', 'AMNDT_IND', 'RPT_TP', 'TRANSACTION_PGI',
 # 2006. Please note, operating expenditures disclosed by paper filers during 
 # the 2006 cycle are only available from reports filed in October 2005 and 
 # later. 
+oppexp <- fec_data_ingest('oppexp')
 
 
-
-oppexp <- read.csv('2016/oppexp.txt', sep = '|', header = F)
-
-
-names(oppexp) <- c('CMTE_ID', 'AMNDT_IND', 'RPT_TP', 'RPT_YR',
-               'TRANSACTION_PGI', 'IMAGE_NUM', 'LINE_NUM',
-               'FORM_TP_CD', 'SCHED_TP_CD', 'NAME', 'CITY',
-               'STATE', 'ZIP_CODE', 'TRANSACTION_DT',
-               'TRANSACTION_AMT', 'TRANSACTION_PGI', 'PURPOSE',
-               'CATEGORY', 'CATEGORY_DESC', 'MEMO_CD',
-               'MEMO_TEXT', 'ENTITY_TP', 'SUB_ID', 'FILE_NUM',
-               'TRAN_ID', 'BACK_REF_TRAN_ID')
 
 # This file contains one record for each candidate to committee linakge.
-CCL_NAMES <- names(read.csv('http://www.fec.gov/finance/disclosure/metadata/ccl_header_file.csv'))
-
-list.files('data') %>%
-  grep('ccl', ., value = T) %>%
-  grep('txt', ., value = T) %>%
-  paste0('data/', .) %>%
-  lapply(read.csv, sep = '|', header = F) %>%
-  bind_rows %>%
-  `names<-`(CCL_NAMES) -> link
+link <- fec_data_ingest('ccl')
 
 # This file contains one record for each committee registered with the Federal 
 # Election Commission. This includes federal political action committees and 
@@ -123,13 +69,8 @@ list.files('data') %>%
 # includes information about what type of committee is being described, along 
 # with the candidate's ID number if it is a campaign committee.
 
-committee <- read.csv('2016/cm.txt', sep = '|', header = F)
+committee <- fec_data_ingest('cm')
 
-names(committee) <- c('CMTE_ID', 'CMTE_NM',  'TRES_NM', 'CMTE_ST1',
-               'CMTE_ST2', 'CMTE_CITY', 'CMTE_ST', 'CMTE_ZIP',
-               'CMTE_DSGN', 'CMTE_TP', 'CMTE_PTY_AFFILIATION',
-               'CMTE_FILING_FREQ', 'ORG_TP', 'CONNECTED_ORG_NM',
-               'CAND_ID')
 
 # This file contains one record for each candidate who has filed the FEC 
 # Form 2 for the upcoming election. In addition, this file includes candidates 
@@ -140,13 +81,7 @@ names(committee) <- c('CMTE_ID', 'CMTE_NM',  'TRES_NM', 'CMTE_ST1',
 # candidate's identification number, candidate's name, office, election year 
 # and address.
 
-candidate <- read.csv('2016/cn.txt', sep = '|', header = F)
-
-names(candidate) <- c('CAND_ID', 'CAND_NAME', 'CAND_PTY_AFFILIATION', 
-               'CAND_ELECTION_YR', 'CAND_OFFICE_ST', 'CAND_OFFICE',
-               'CAND_OFFICE_DISTRICT', 'CAND_ICI', 'CAND_STATUS',
-               'CAND_PCC', 'CAND_ST1', 'CAND_ST2', 'CAND_CITY',
-               'CAND_ST', 'CAND_ZIP')
+candidate <- fec_data_ingest('cn') 
 
 
 

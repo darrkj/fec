d299563b300124ec7ce7b27d52702f309a570141
Kenny Darrell <darrelkj@gmail.com>
2015-09-12 09:45:07 -0400

    pull by function works

--- fec_import.R
+++ fec_import.R
@@ -3,7 +3,7 @@ library(dplyr)
 # Data comes from
 # http://www.fec.gov/finance/disclosure/ftpdet.shtml
 
-fec_data_ingest <- function(str) {
+fec_data_ingest <- function(str, newest = F) {
   str %>%
     #paste0('http://www.fec.gov/finance/disclosure/metadata/', ., '_header_file.csv') %>%
     paste0('names/', ., '_header_file.csv') %>%
@@ -11,10 +11,11 @@ fec_data_ingest <- function(str) {
     names -> data_names
   
   list.files('data') %>%
-    grep('ccl', ., value = T) %>%
+    grep(str, ., value = T) %>%
     grep('txt', ., value = T) %>%
+    `if`(newest, grep('16', ., value = T), .)  %>%
     paste0('data/', .) %>%
-    lapply(read.csv, sep = '|', header = F) %>%
+    lapply(function(x) {print(x); read.csv(x, sep = '|', header = F)}) %>%
     bind_rows %>%
     `names<-`(data_names)
 }
@@ -24,7 +25,7 @@ fec_data_ingest <- function(str) {
 # The individual contributions file contains each contribution from an 
 # individual to a federal committee if the contribution was at least $200.
 
-indiv <- fec_data_ingest('itcont')
+indiv <- fec_data_ingest('indiv', T)
 
 
 # The contributions to committees from committees file contains each 
@@ -32,7 +33,7 @@ indiv <- fec_data_ingest('itcont')
 # candidate committee, or other federal committee to another committee during 
 # the two-year election cycle.
 
-cm2cm <- fec_data_ingest('itoth') 
+cm2cm <- fec_data_ingest('oth', T) 
 
 # The contributions to candidates from committees file contains each 
 # contribution or independent expenditure made by a PAC, party committee, 
@@ -40,7 +41,7 @@ cm2cm <- fec_data_ingest('itoth')
 # two-year election cycle.
 
 
-cm2cd <- fec_data_ingest('itpas2')
+cm2cd <- fec_data_ingest('pas2', T)
 
 # The Operating Expenditures file contains disbursements reported on FEC 
 # Form 3 Line 17, FEC Form 3P Line 23and FEC Form 3X Lines 21(a)(i), 21(a)(ii) 
@@ -51,12 +52,12 @@ cm2cd <- fec_data_ingest('itpas2')
 # 2006. Please note, operating expenditures disclosed by paper filers during 
 # the 2006 cycle are only available from reports filed in October 2005 and 
 # later. 
-oppexp <- fec_data_ingest('oppexp')
+oppexp <- fec_data_ingest('oppexp', T)
 
 
 
 # This file contains one record for each candidate to committee linakge.
-link <- fec_data_ingest('ccl')
+link <- fec_data_ingest('ccl', T)
 
 # This file contains one record for each committee registered with the Federal 
 # Election Commission. This includes federal political action committees and 
@@ -69,7 +70,7 @@ link <- fec_data_ingest('ccl')
 # includes information about what type of committee is being described, along 
 # with the candidate's ID number if it is a campaign committee.
 
-committee <- fec_data_ingest('cm')
+committee <- fec_data_ingest('cm', T)
 
 
 # This file contains one record for each candidate who has filed the FEC 
@@ -81,7 +82,7 @@ committee <- fec_data_ingest('cm')
 # candidate's identification number, candidate's name, office, election year 
 # and address.
 
-candidate <- fec_data_ingest('cn') 
+candidate <- fec_data_ingest('cn', T) 
 
 
 
